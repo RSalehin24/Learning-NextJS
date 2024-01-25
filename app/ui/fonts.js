78ed7bf90f7a85972938d5c017053c627a89a389
Md. Reyanus Salehin <reyanus@nascenia.com>
2024-01-25 12:34:45 +0600

    initial commit with some changes

---
+++ app/ui/fonts.js
@@ -0,0 +1,6 @@
+// import LusitanaRegular from '../../public/fonts/Lusitana-Regular.ttf';
+import { Inter } from 'next/font/google'
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] })
+export const lusitana = Lusitana({ subsets: ['latin'], weight: "400" })
